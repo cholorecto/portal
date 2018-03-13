90dbd17d6a0e552bec20a4766a32e26a65e9e60a
cholorecto <pocholorecto123@gmail.com>
2018-03-13 10:46:45 +0800

    Minor fixes

--- assets/js/invoice/invoice.controller.js
+++ assets/js/invoice/invoice.controller.js
@@ -1,16 +1,31 @@
+/* INVOICE CONTROLLER
+ * @desc : This module will handle the data from the backend
+ */
 var InvoiceController = function () {
-
-  // Manipulate data from invoice service
-
+  // Local variable
   var controller = {
       detail:displayDetail,
+      defaultInvoice:defaultDisplay
     }
 
-  $('#invoice-detail').load('/static/js/invoice/templates/invoice_detail.html');
-  function displayDetail (inv_id) {
-    // Handle the invoice detail
-    var invoiceDetail = InvoiceService.detail(inv_id);
+    /* Load Invoice Default Details
+   * @desc : Sets the default display of the invoice detail
+   */
+  function defaultDisplay () {
+    var invoiceId = $('.invoice-item').first().attr('id');
+    if (invoiceId) controller.detail(invoiceId);
+  }
 
+  /* Load Invoice Details
+   * @desc : Loads the invoice information into the template
+   */
+  function displayDetail (invoiceId) {
+    // This will load html javacsript template
+    $('#invoice-details').load('/static/js/invoice/templates/invoice_detail.html');
+    // Initialized the invoice service getDetail function
+    var invoiceDetail = InvoiceService.detail(invoiceId);
+
+    // Gets the detail of invoice
     invoiceDetail
       .done(function (response) {
         var context = {
@@ -19,19 +34,18 @@ var InvoiceController = function () {
           notes: response.notes,
           total_amount: response.total_amount
         };
+
+        // Loads the data in the template
         var tmpl = _.template($('#invoiceTemplate').html());
-        $('.invoice-view-details').html(tmpl(context));
+        $('.invoice-detail').html(tmpl(context));
       })
   }
 
   return controller;
 
-}
+}();
 
-var invoiceCtrl = InvoiceController();
-
-// Sets the default display of the invoice detail
-$(function () {
-  var inv_id = $('.invoice-item').first().attr('id');
-  if (inv_id) invoiceCtrl.detail(inv_id);
-});
+// Initialized the invoice controller
+var invoiceCtrl = InvoiceController;
+// Calls the function that assigns the default invoice detail
+InvoiceController.defaultInvoice();
